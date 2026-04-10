4198e50b385490f89866cf3c55de47650291de17
Derek Fredrickson <dfreds08@gmail.com>
2026-04-10 10:33:03 -0400

    Fix showing effects in combat tracker

--- src/ts/hooks/setup.ts
+++ src/ts/hooks/setup.ts
@@ -3,6 +3,7 @@ import { libWrapper } from "@static/lib/shim.ts";
 import { MODULE_ID } from "../constants.ts";
 import { Listener } from "./index.ts";
 import { HiddenTokenEffects } from "../hidden-token-effects.ts";
+import CombatTracker from "@client/applications/sidebar/tabs/combat-tracker.mjs";
 
 const Setup: Listener = {
     listen(): void {
@@ -23,6 +24,13 @@ const Setup: Listener = {
                 "ActiveEffect.prototype._displayScrollingStatus",
                 displayScrollingStatusWrapper,
                 "MIXED",
+            );
+
+            libWrapper.register(
+                MODULE_ID,
+                "CombatTracker.prototype._prepareTurnContext",
+                prepareTurnContextWrapper,
+                "OVERRIDE",
             );
         });
     },
@@ -86,6 +94,52 @@ async function displayScrollingStatusWrapper(
         wrapped(enabled)
         return;
     }
+}
+
+async function prepareTurnContextWrapper(
+    this: CombatTracker,
+    combat: Combat,
+    combatant: Combatant,
+    index: number,
+) {
+    const { id, name, isOwner, isDefeated, hidden, initiative, permission } = combatant;
+    const resource = permission >= CONST.DOCUMENT_OWNERSHIP_LEVELS.OBSERVER ? combatant.resource : null;
+    const hasDecimals = Number.isFinite(initiative) && !Number.isInteger(initiative);
+    const turn = {
+        hasDecimals, hidden, id, isDefeated, initiative, isOwner, name, resource,
+        active: index === combat.turn,
+        canPing: (combatant.sceneId === canvas.scene?.id) && game.user.hasPermission("PING_CANVAS"),
+        img: await this._getCombatantThumbnail(combatant)
+    };
+    // @ts-ignore
+    turn.css = [
+        turn.active ? "active" : null,
+        hidden ? "hide" : null,
+        isDefeated ? "defeated" : null
+    ].filterJoin(" ");
+    const effects = [];
+    const SHOW_ICON = CONST.ACTIVE_EFFECT_SHOW_ICON;
+    for (const effect of combatant.actor?.appliedEffects ?? []) {
+        const hiddenTokenEffects = new HiddenTokenEffects();
+
+        if (effect.statuses.has(CONFIG.specialStatusEffects.DEFEATED)) turn.isDefeated = true;
+        /* Added Code Start */
+        else if (!(await hiddenTokenEffects.shouldShowEffect(effect))) {
+            continue;
+        }
+        /* Added Code End */
+        else if ((effect.showIcon === SHOW_ICON.ALWAYS)
+            || ((effect.showIcon === SHOW_ICON.CONDITIONAL) && effect.isTemporary)) {
+            effects.push({ img: effect.img, name: effect.name });
+        }
+    }
+    // @ts-ignore
+    turn.effects = {
+        icons: effects,
+        // @ts-ignore
+        tooltip: this._formatEffectsTooltip(effects)
+    };
+    return turn;
 }
 
 export { Setup };
